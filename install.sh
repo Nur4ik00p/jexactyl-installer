f2da60da328d6c81e04511d4810f18f2a4acafb0
Nur4ik00p <161104383+Nur4ik00p@users.noreply.github.com>
2024-12-09 16:36:08 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -30,7 +30,7 @@ set -e
 
 export GITHUB_SOURCE="v1.1.1"
 export SCRIPT_RELEASE="v1.1.1"
-export GITHUB_BASE_URL="https://raw.githubusercontent.com/Nur4ik00p/jexactyl-installer"
+export GITHUB_BASE_URL="https://raw.githubusercontent.com/Nur4ik00p/jexactyl-installer/main"
 
 LOG_PATH="/var/log/pterodactyl-installer.log"
 
